c672035d6d11d4e1d72a000638ec91dde2083709
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-21 18:20:59 -0700

    Created server

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,7 +1,14 @@
 import express from "express"
+import toplevelRouter from "../routers/toplevel"
 
 const app = express()
+app.set("trust proxy", 1)
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+app.use(toplevelRouter)
 app.listen(4000, () => {
     console.log("Listening on port 4000")
 })
