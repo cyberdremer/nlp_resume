15b37845de32af1fc18021940275a1cbff882e84
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-22 21:52:23 -0700

    Added session middleware to server

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,19 +1,24 @@
-import express from "express"
-import logger from "../middleware/logger"
-import toplevelRouter from "../routers/toplevel"
-import "dotenv/config"
+import express from "express";
+import passport from "../config/passport";
+import sessionConfig from "../config/session";
+import logger from "../middleware/logger";
+import toplevelRouter from "../routers/toplevel";
+import "dotenv/config";
 
-const app = express()
-app.set("trust proxy", 1)
+const app = express();
+app.set("trust proxy", 1);
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
 
-app.use(logger)
-app.use(toplevelRouter)
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(sessionConfig);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(logger);
+app.use(toplevelRouter);
 app.listen(process.env.SERVER_PORT, () => {
-    console.log("Listening on port 4000")
-})
+  console.log("Listening on port 4000");
+});
 
-
-export default app
+export default app;
