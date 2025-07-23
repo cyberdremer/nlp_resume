cc8361605ad1d99e046f43e65f2713eb1076379a
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-22 21:51:55 -0700

    Added top level router

--- backend/src/routers/toplevel.ts
+++ backend/src/routers/toplevel.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import signUpRouter from "./signup";
+import loginRouter from "./login";
 const toplevelRouter = Router()
 
 toplevelRouter.use("/signup", signUpRouter)
+toplevelRouter.use("/login", loginRouter)
 
 
 export default toplevelRouter;
