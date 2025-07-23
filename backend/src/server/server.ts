56062accc61e370cc2b12cc23038943368cc1fd5
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-22 20:18:44 -0700

    Added export to test and logger middleware

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,5 +1,7 @@
 import express from "express"
+import logger from "../middleware/logger"
 import toplevelRouter from "../routers/toplevel"
+import "dotenv/config"
 
 const app = express()
 app.set("trust proxy", 1)
@@ -7,8 +9,11 @@ app.set("trust proxy", 1)
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-
+app.use(logger)
 app.use(toplevelRouter)
-app.listen(4000, () => {
+app.listen(process.env.SERVER_PORT, () => {
     console.log("Listening on port 4000")
 })
+
+
+export default app
