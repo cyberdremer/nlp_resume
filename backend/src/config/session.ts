61c4fdab1864f0783839fe2a3268f68b975eddba
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-22 21:47:13 -0700

    Added session middleware

---
+++ backend/src/config/session.ts
@@ -0,0 +1,28 @@
+import prisma from "./prisma";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import session from "express-session";
+import "dotenv/config"
+
+
+const sessionConfig = session({
+    secret: process.env.SESSION_SECRET ?? "",
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "test" ? false : true,
+        sameSite: process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "test" ? "lax" : "none",
+        httpOnly: process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "test" ? false : true,
+    },
+
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+        checkPeriod: 2 * 60 * 1000,
+        logger: console
+    })
+})
+
+
+export default sessionConfig
+
+
+
