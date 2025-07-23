931b26c13feb8545310b50231859958bab58b9b2
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-22 21:48:20 -0700

    added programatically db url

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from "@prisma/client"
 import "dotenv/config"
 
+
+const dbUrl = process.env.NODE_ENV === "dev" ? process.env.DEV_DATABASE_URL : process.env.TEST_DATABASE_URL
+
 const prisma = new PrismaClient({
     datasources: {
         db: {
-            url: process.env.DEV_DATABASE_URL
+            url: dbUrl
         }
     }
 })
