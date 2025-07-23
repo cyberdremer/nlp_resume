e21ee29f9a85645c8a04690256c0735a3b21212e
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-22 21:49:36 -0700

    Added login router

---
+++ backend/src/routers/login.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import loginController from "../controllers/login";
+const loginRouter = Router()
+loginRouter.post("/", loginController)
+
+export default loginRouter
