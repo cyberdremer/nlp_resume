b5a00430f23305948d9cf81abd96d94905afbd9d
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-24 21:08:20 -0700

    Created functions to upload to cloudinary

---
+++ backend/src/cloud/uploading.ts
@@ -0,0 +1,83 @@
+import {
+  UploadStream,
+  UploadResponseCallback,
+  UploadApiErrorResponse,
+  UploadApiResponse,
+} from "cloudinary";
+import cloudUploader from "../config/cloudinary";
+import {
+  SuccessfullServerResponse,
+  SuccessResponse,
+} from "../interfaces/successresponse";
+import { resolve } from "path";
+import { idText, PrimaryExpression } from "typescript";
+import { BadRequest, ResourceNotFoundError } from "../errors/specificerrors";
+
+const uploadBufferToCloudinary = async (
+  username: string,
+  buffer: Buffer
+): Promise<UploadApiResponse> => {
+  const uploadResult: UploadApiResponse = await new Promise(
+    (resolve, reject) => {
+      cloudUploader.uploader
+        .upload_stream(
+          {
+            folder: `nlp_resume_app/${username}`,
+            type: "auto",
+          },
+          (error, result) => {
+            if (error) {
+              return reject(new BadRequest(error.message));
+            }
+            if (!result) {
+              return reject(new BadRequest("Upload failed with no result!"));
+            }
+            resolve(result);
+          }
+        )
+        .end(buffer);
+    }
+  );
+  return uploadResult;
+};
+
+const deleteFileFromCloudinary = async (fileId: string) => {
+  try {
+    const deletedFile = await cloudUploader.uploader.destroy(fileId);
+    return deletedFile;
+  } catch (error) {
+    throw new ResourceNotFoundError(
+      `File with file id: ${fileId}, was not found!`
+    );
+  }
+};
+
+const replaceFileFromCloudinary = async (
+  fileId: string,
+  buffer: Buffer
+): Promise<UploadApiResponse> => {
+  const uploadResult: UploadApiResponse = await new Promise(
+    (resolve, reject) => {
+      cloudUploader.uploader
+        .upload_stream({ public_id: fileId }, (err, result) => {
+          if (err) {
+            return reject(new BadRequest(err.message));
+          }
+          if (!result) {
+            return reject(new BadRequest("Error with the file stream"));
+          }
+
+          resolve(result);
+        })
+        .end(buffer);
+    }
+  );
+
+  return uploadResult;
+};
+
+export {
+  uploadBufferToCloudinary,
+  deleteFileFromCloudinary,
+  replaceFileFromCloudinary,
+};
