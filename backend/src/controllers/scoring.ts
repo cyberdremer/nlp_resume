3c524b88da4eaa7801c4a8789c6595cf8298c7e7
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-26 15:36:54 -0700

    Finshed routes related to scoring resumes

--- backend/src/controllers/scoring.ts
+++ backend/src/controllers/scoring.ts
@@ -2,11 +2,77 @@ import { Response, Request, NextFunction, RequestHandler } from "express";
 import asyncHandler from "express-async-handler";
 import isAuthorized from "../middleware/authorized";
 import PdfParse from "pdf-parse";
+import generateEmbedding from "../config/openai";
+import prisma from "../config/prisma";
+import {
+  computeCosineSimilarity,
+  writeEmbeddingToTable,
+} from "../utility/rawsql/rawstatements";
+import { computeSimilarityRating } from "../utility/similarityscore";
+import { SuccessfullServerResponse } from "../interfaces/successresponse";
 
-const resumeEmbeddingController: RequestHandler[] = [
+const resumeScoreController: RequestHandler[] = [
   isAuthorized,
   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
+    const { resumeId } = req.params;
     const userid = req.user.id;
+    const { jobDescription, title } = req.body;
+    const jobDescriptionEmbeddings = await generateEmbedding(jobDescription);
+
+    const resumeRecord = await prisma.resume.findFirstOrThrow({
+      where: {
+        userid: userid,
+        id: Number(resumeId),
+      },
+    });
+
+    const jobDescriptionRecord = await prisma.jobDescription.create({
+      data: {
+        content: jobDescription,
+        created: new Date(),
+        userId: userid,
+        title: title,
+      },
+    });
+
+    const affectedRowsJD = await writeEmbeddingToTable(
+      "JobDescription",
+      jobDescriptionRecord.id,
+      jobDescriptionEmbeddings
+    );
+    const similarity_score = await computeCosineSimilarity(
+      jobDescriptionRecord.id,
+      resumeRecord.id
+    );
+    const ratingObject = computeSimilarityRating(
+      similarity_score.similarityScore
+    );
+
+    const feedBackRecord = await prisma.feedback.create({
+      data: {
+        date: new Date(),
+        name: `${resumeRecord.name + jobDescriptionRecord.title}`,
+        text: ratingObject.match,
+        score: ratingObject.rating,
+        resumeId: Number(resumeId),
+        jobId: jobDescriptionRecord.id,
+      },
+    });
+
+    const successResponse: SuccessfullServerResponse<{}> = {
+      data: {
+        message: "Your resume has been scored!",
+        status: 201,
+        object: ratingObject,
+      },
+    };
+
+    res.status(successResponse.data.status).json(successResponse);
   }),
 ];
+
+
+
+export {resumeScoreController}
+
+
