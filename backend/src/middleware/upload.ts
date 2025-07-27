09a494f9edaa348b75c7f16abbefd5c74d3bc69c
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-27 12:07:22 -0700

    Changed file size limit to be variable, no longer hardcoded

--- backend/src/middleware/upload.ts
+++ backend/src/middleware/upload.ts
@@ -1,9 +1,10 @@
 import multer from "multer";
-import { Request} from "express";
+import { Request } from "express";
 import { FileFilterCallback } from "multer";
 import { UnsupportedFileType, ValidationError } from "../errors/specificerrors";
 
 const storage = multer.memoryStorage();
+const fileSizeLimit = 5 * 1024 * 1024;
 
 const fileFilter = (
   req: Request,
@@ -19,14 +20,16 @@ const fileFilter = (
   if (!req.file) {
     cb(new ValidationError(`File: you cannot upload an empty field!`));
   }
+
   if (!acceptedTypes.includes(file.mimetype)) {
     cb(new UnsupportedFileType(file.mimetype));
   }
+
   cb(null, true);
 };
 
 const limits = {
-  fileSize: 10 * 1024 * 1024,
+  fileSize: fileSizeLimit,
 };
 
 const multerMiddleware = multer({
