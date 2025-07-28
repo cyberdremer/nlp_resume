d0ba92f242ab635a18d739e5c1754daa3864b3a4
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-28 16:53:49 -0700

    Created route to score resumes

--- backend/src/routers/scoring.ts
+++ backend/src/routers/scoring.ts
@@ -1,1 +1,8 @@
 import { Router } from "express";
+import { resumeScoreController } from "../controllers/scoring";
+
+const scoreRouter = Router()
+
+scoreRouter.get(":id/score/:resumeId", resumeScoreController)
+
+export default scoreRouter
