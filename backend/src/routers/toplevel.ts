2ab1df8ab099a9dd8026179ed3d695690c1cb39e
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-07-28 16:59:14 -0700

    Added resume routes to top level router

--- backend/src/routers/toplevel.ts
+++ backend/src/routers/toplevel.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import signUpRouter from "./signup";
 import loginRouter from "./login";
+import resumeRouter from "./resume";
 const toplevelRouter = Router()
 
 toplevelRouter.use("/signup", signUpRouter)
 toplevelRouter.use("/login", loginRouter)
+toplevelRouter.use("/resume", resumeRouter)
 
 
 export default toplevelRouter;
